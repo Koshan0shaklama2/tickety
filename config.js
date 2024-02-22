77339658e649509f44fdc050a8b4bdb596ed3794
Koshan0shaklama2 <124837818+Koshan0shaklama2@users.noreply.github.com>
2024-02-22 21:37:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["721374461970087986", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "00fbff",
